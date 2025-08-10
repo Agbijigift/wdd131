7c1a5b5744040538d2707f04fae4efaf1b3d432a
Gift Agbiji <giftagbiji8@gmail.com>
2025-08-10 22:26:08 +0100

    done

--- project/script/script.js
+++ project/script/script.js
@@ -19,4 +19,44 @@ menu.addEventListener("click", () =>{
 })
 
 
+document.addEventListener("DOMContentLoaded", () => {
+    const userPreferences = {
+        theme: "light",
+        welcomeMessage: "Welcome to Constructaora"
+    };
+
+    const heroImages = [
+        "./images/hero2.webp",
+        "./images/hero7.webp",
+        "./images/hero4.webp"
+    ];
+
+    if (localStorage.getItem("visitedBefore")) {
+        const username = localStorage.getItem("username") || "Guest";
+        alert(`Welcome back, ${username}!`);
+    } else {
+        const name = prompt("Welcome! What's your name?");
+        if (name) {
+            localStorage.setItem("username", name);
+            alert(`Nice to meet you, ${name}!`);
+        }
+        localStorage.setItem("visitedBefore", true);
+    }
+
+    const heroImgElement = document.querySelector(".hero img");
+    let currentHeroIndex = 0;
+
+    heroImgElement.addEventListener("click", () => {
+        currentHeroIndex = (currentHeroIndex + 1) % heroImages.length;
+        heroImgElement.src = heroImages[currentHeroIndex];
+        console.log(`Hero image changed to: ${heroImages[currentHeroIndex]}`);
+    });
+
+    heroImages.forEach((img, index) => {
+        console.log(`Image ${index + 1}: ${img}`);
+    });
+});
+
+
+
 
